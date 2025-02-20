cec626390ebe8e70841d9f329821f2b9acb49abe
ONG'AYI JOHNIAN <ongayijohnian@gmail.com>
2025-02-20 17:38:29 +0300

    Add SQLite support and Customer model
    - Updated `.gitignore` to ignore SQLite database files.
    - Introduced `Customer` class with validation attributes.
    - Created `RepositoryContext` for managing customer records and seeding initial data.
    - Added `DbService` for configuring SQLite context in dependency injection.
    - Updated `SimpleCRM.csproj` with necessary Entity Framework and SQLite package references.
    - Added connection string for SQLite database in `appsettings.json`.

---
+++ Entities/Models/Customer.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCRM.Entities.Models
+{
+	public class Customer
+	{
+		public int Id { get; set; }
+
+		[Required(ErrorMessage = "First name is required")]
+		[StringLength(60, ErrorMessage = "First name cannot be longer than 60 characters")]
+		public string? FirstName { get; set; }
+
+		[Required(ErrorMessage = "Last name is required")]
+		[StringLength(60, ErrorMessage = "Last name cannot be longer than 60 characters")]
+		public string? LastName { get; set; }
+
+		[Required(ErrorMessage = "Phone number is required")]
+		[StringLength(16, ErrorMessage = "Invalid Phone number")]
+		public string? Phone { get; set; }
+
+		public string? City { get; set; }
+
+		public string? State { get; set; }
+
+		public string? Address { get; set; }
+	}
+}
---
+++ Extensions/DbService.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SimpleCRM.Entities;
+
+namespace SimpleCRM.Extensions
+{
+	public static class DbService
+	{
+		public static void ConfigureSqliteContext(this IServiceCollection services, IConfiguration config)
+		{
+			services.AddDbContext<RepositoryContext>(options =>
+	options.UseSqlite(config.GetConnectionString("sqliteConnection")));
+		}
+	}
+}
