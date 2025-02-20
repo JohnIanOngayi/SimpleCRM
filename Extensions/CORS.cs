f0b07286cf7c9d21648447fbd3470845f78f0909
ONG'AYI JOHNIAN <ongayijohnian@gmail.com>
2025-02-20 14:51:05 +0300

    Add CORS support and update API documentation settings
    - Introduced CORS configuration in `CORS.cs` with a method to allow any origin, method, and header.
    - Updated `Program.cs` to call the new CORS method and modified service registration order.
    - Implemented a custom Swagger route template and added Scalar API reference mapping.
    - Changed default launch URL in `launchSettings.json` to "scalar/v1" and updated application URLs to ports 5000 and 5001.
    - Added `Scalar.AspNetCore` package reference in `SimpleCRM.csproj` for new features.

---
+++ Extensions/CORS.cs
@@ -0,0 +1,21 @@
+﻿namespace SimpleCRM.Extensions
+{
+	/// <summary>
+	/// Provides extension methods for configuring CORS in the application.
+	/// </summary>
+	public static class CORS
+	{
+		/// <summary>
+		/// Configures Cross-Origin Resource Sharing (CORS) for the application.
+		/// </summary>
+		/// <param name="services">The IServiceCollection to add the CORS services to.</param>
+		public static void ConfigureCORS(this IServiceCollection services)
+		{
+			services.AddCors(options =>
+			{
+				options.AddPolicy("CorsPolicy",
+					builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+			});
+		}
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,25 +1,48 @@
-var builder = WebApplication.CreateBuilder(args);
+using SimpleCRM.Extensions;
+using Scalar.AspNetCore;
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+internal class Program
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+	private static void Main(string[] args)
+	{
+		var builder = WebApplication.CreateBuilder(args);
+
+		// Add services to the container.
+
+		builder.Services.ConfigureCORS();
+		builder.Services.AddControllers();
+		// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+		builder.Services.AddEndpointsApiExplorer();
+		builder.Services.AddSwaggerGen();
+
+		var app = builder.Build();
+
+		// Configure the HTTP request pipeline.
+			
+		// Configuring openapi Scala
+		app.UseSwagger(options => options.RouteTemplate = "openapi/{documentName}.json");	
+		app.MapScalarApiReference(options =>
+		{
+			options.Title = "Simple CRM";
+			options.Theme = ScalarTheme.Mars;
+			options.DefaultHttpClient = new(ScalarTarget.Http, ScalarClient.Http11);
+		});
+
+		// app.UseHttpsRedirection();
+
+		app.UseStaticFiles();
+
+		app.UseForwardedHeaders(new ForwardedHeadersOptions
+		{
+			ForwardedHeaders = Microsoft.AspNetCore.HttpOverrides.ForwardedHeaders.All
+		});
+
+		app.UseCors("CorsPolicy");
+
+		app.UseAuthorization();
+
+		app.MapControllers();
+
+		app.Run();
+	}
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
